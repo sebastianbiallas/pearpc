ba27a58209ba843788b191b2c84cc31db05179bc
seppel <seppel>
2004-06-24 21:32:26 +0000

    .

--- src/system/osapi/win32/systhread.cc
+++ src/system/osapi/win32/systhread.cc
@@ -107,7 +107,7 @@ void sys_wait_semaphore(sys_semaphore s)
 
 void sys_wait_semaphore_bounded(sys_semaphore s, int ms)
 {
-	WaitForSingleObject(((sys_win32_semaphore *)s)->sem, INFINITE);
+	WaitForSingleObject(((sys_win32_semaphore *)s)->sem, ms);
 }
 
 void sys_lock_semaphore(sys_semaphore s)
