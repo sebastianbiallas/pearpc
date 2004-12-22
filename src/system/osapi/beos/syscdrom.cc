a7841da767bd9994b4e7efec53c6edad36afff1f
mmu_man <mmu_man>
2004-12-22 22:56:01 +0000

    real native (SCSI) CDROM support \o/

--- src/system/osapi/beos/syscdrom.cc
+++ src/system/osapi/beos/syscdrom.cc
@@ -20,7 +20,9 @@
 
 #include <cstdio>
 #include <cstring>
-#include <Drivers.h>
+#include <device/scsi.h>
+#include <drivers/Drivers.h>
+#include <drivers/CAM.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "errno.h"
@@ -28,42 +30,88 @@
 #include "debug/tracers.h"
 #include "io/ide/cd.h"
 
+#define IO_NCD_TRACE(msg...) IO_IDE_TRACE("[CDROM/BEOS] "msg)
+#define IO_NCD_WARN(msg...) IO_IDE_WARN("[CDROM/BEOS] "msg)
+
+#define SCSI_CMD_DIR_IN 1
+#define SCSI_CMD_DIR_OUT 2
+
+#define CD_FRAMESIZE 2048
+
 /// BeOS ATAPI-based CD-ROM implementation
-class CDROMDeviceBeOS:public CDROMDeviceFile
+class CDROMDeviceBeOS:public CDROMDeviceSCSI
 {
+private:
+	int fd;
 public:
 	 CDROMDeviceBeOS(const char *name);
 	virtual ~CDROMDeviceBeOS();
+	virtual byte SCSI_ExecCmd(byte command, byte dir, byte params[11], byte *buffer, unsigned int buffer_len);
 
-	bool changeDataSource(const char *file);
 	virtual void eject();
 };
 
 CDROMDeviceBeOS::CDROMDeviceBeOS(const char *name)
-	: CDROMDeviceFile(name)
+	: CDROMDeviceSCSI(name)
 {
-	changeDataSource(name); // base class doesn't open on create
+	IO_NCD_TRACE("%s()\n", __FUNCTION__);
+	fd = ::open(name, O_RDONLY);
 }
 
 CDROMDeviceBeOS::~CDROMDeviceBeOS()
 {
+	::close(fd);
 }
 
-bool CDROMDeviceBeOS::changeDataSource(const char *file)
+/// SCSI command pass-through function
+/// @author Alexander Stockinger
+/// @date 07/18/2004
+/// @param command The SCSI command to be sent
+/// @param dir The data direcion flags
+/// @param params byte[11] array containing command dependent parameters
+/// @param buffer Buffer for data exchange
+/// @param buffer_len The size of buffer in bytes
+/// @return If the call could be executed it returns the status from the device, else it returns 0xff
+byte CDROMDeviceBeOS::SCSI_ExecCmd(byte command, byte dir, byte params[11], byte *buffer, unsigned int buffer_len)
 {
-	return CDROMDeviceFile::changeDataSource(file);
+	raw_device_command cmd;
+	int ret;
+	int i;
+	
+	memset(&cmd, 0, sizeof(cmd));
+	cmd.flags = B_RAW_DEVICE_SHORT_READ_VALID;
+	if (dir & SCSI_CMD_DIR_IN)
+		cmd.flags |= B_RAW_DEVICE_DATA_IN;
+
+	cmd.command[0] = command;
+	if (command < 0x20)
+		cmd.command_length = 6;
+	else if (command < 0xa0)
+		cmd.command_length = 10;
+	else
+		cmd.command_length = 12;
+	//cmd.command_length = 10; //XXX: force
+	for (i = 0; i + 1 < cmd.command_length; i++)
+		cmd.command[i+1] = params[i];
+	cmd.data = buffer;
+	cmd.data_length = buffer_len;
+	cmd.timeout = 5000000;
+	ret = ::ioctl(fd, B_RAW_DEVICE_COMMAND, &cmd, sizeof(cmd));
+	//IO_NCD_TRACE("command done. scsi status: 0x%08lx cam status: 0x%08lx\n", cmd.scsi_status, cmd.cam_status);
+	if (ret < 0 /*|| cmd.scsi_status || cmd.cam_status*/) {
+		IO_NCD_WARN("ioctl() returned error 0x%08lx (%s)\n", errno, strerror(errno));
+		IO_NCD_WARN("command: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x:\n",
+		command, params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9]);
+		return 0xff;
+	}
+	return cmd.scsi_status;
 }
+
 
 void CDROMDeviceBeOS::eject()
 {
-	//if (!mFile) return; // no file open
-	// annoying: mFile is private, must open() again...
-	int fd = open(mName, O_RDWR);
-	if (fd < 0)
-		return;
-	ioctl(fd, B_EJECT_DEVICE, NULL, 0);
-	// we don't care if it worked (might be a regular file)
-	close(fd);
+	IO_NCD_TRACE("%s()\n", __FUNCTION__);
+	::ioctl(fd, B_EJECT_DEVICE, NULL, 0);
 }
 
 /// Creates a native CDROM device
@@ -74,22 +122,22 @@ void CDROMDeviceBeOS::eject()
 /// @date 07/19/2004
 CDROMDevice* createNativeCDROMDevice(const char* device_name, const char* image_name)
 {
-	// IO_IDE_WARN("No native CDROMs supported on BeOS\n");
+	// IO_NCD_WARN("No native CDROMs supported on BeOS\n");
 	// check for a real cdrom drive...
 	device_geometry geom;
 	int err;
-	int fd = open(device_name, O_RDONLY);
+	int fd = open(image_name, O_RDONLY);
 	err = ioctl(fd, B_GET_GEOMETRY, &geom, sizeof(geom));
 	close(fd);
 	if (err < 0) {
-		IO_IDE_WARN("Can't open CDROM '%s'\n", device_name);
+		IO_NCD_WARN("Can't open CDROM '%s'\n", image_name);
 		return NULL;
 	}
 	if (geom.device_type != B_CD) {
-		IO_IDE_WARN("File '%s' not a CDROM device\n", device_name);
+		IO_NCD_WARN("File '%s' not a CDROM device\n", image_name);
 		return NULL;
 	}
 	// create it
-	CDROMDevice *dev = new CDROMDeviceBeOS(device_name);
+	CDROMDevice *dev = new CDROMDeviceBeOS(image_name);
 	return dev;
 }
